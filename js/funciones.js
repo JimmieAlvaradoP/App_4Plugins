81a552d4064ef2a7b58a70cff8f37f4a61af3fc1
Jimmie <jimmie.alvarado@outlook.com>
2018-05-17 03:35:46 -0400

    Se añade aplicacion de mensaje de texto

--- js/funciones.js
+++ js/funciones.js
@@ -2,106 +2,111 @@
 
 var pictureSource;   // picture source
 var destinationType; // sets the format of returned value
+var lat = '-33.4349083';
+var lon = '-70.6170132';
 document.addEventListener("deviceready", onDeviceReady, false);
 
-function showAlert(msj)
-{
-    navigator.notification.alert(
-        msj,  // message
-        'UNAB',   // title
-        ''    // buttonName
-    );
+function showAlert(msj){
+  navigator.notification.alert(
+    msj,  // message
+    'UNAB',   // title
+    ''    // buttonName
+  );
 }//fin function mensaje.
 
 // PhoneGap is ready
-    function onDeviceReady() 
-    {
-		// Do cool things here...
-		document.getElementById('largeImage').src='';
-		clearCache();
-		pictureSource=navigator.camera.PictureSourceType;
-		destinationType=navigator.camera.DestinationType;
-    }
-    function clearCache() 
-    {
-		navigator.camera.cleanup();
-	}
- 
-	function getImage(source) 
-	{
-	    // Retrieve image file location from specified source
-		navigator.camera.getPicture(uploadPhoto, onFail, { quality: 50,
-    destinationType: Camera.DestinationType.DATA_URL, sourceType: source});	//destinationType: navigator.camera.DestinationType.FILE_URI
-	 
-    }
-    
-    function onFail(message) {
-    
-    clearCache();
-		//alert('Captura Descartada.');
-		showAlert('Captura Descartada.'+ message);
-			
+function onDeviceReady(){
+  // Do cool things here...
+  document.getElementById('largeImage').src='';
+  clearCache();
+  pictureSource=navigator.camera.PictureSourceType;
+  destinationType=navigator.camera.DestinationType;
+  if (! SMS ) { alert( 'SMS plugin not ready' ); return; } //SMS
 }
- 
-    function uploadPhoto(imageURI) 
-    {
-	  var largeImage = document.getElementById('largeImage');
-	    largeImage.style.display = 'block';
-	    largeImage.src ="data:image/jpeg;base64," + imageURI;
-	    
+
+function clearCache(){
+  navigator.camera.cleanup();
+}
+
+function sendSMS(){
+  var fono=document.getElementById('fono').value;
+  var mensajetexto=document.getElementById('mensajetexto').value;
+  if(fono==''){
+    showAlert('¡Debe Ingresar el Fono!');
+  }
+  else{
+    if(mensajetexto==""){
+      showAlert('¡Ingrese un texto como mensaje!');
     }
-    
-   
-function enviaFoto()
-{
-	var user=document.getElementById('user').value;
-	//var foto=document.getElementById('largeImage');
-	var fotoSrc=document.getElementById('largeImage').src;
-	
-	if(user=='' || fotoSrc=='')
-	{
-		showAlert('Debe Ingresar los valores!');
-	}
-	else
-	{
-	  //var fotoCod=encodeImageFileAsURL(foto);
+    else{
+      var textoURl = "y no tengo GPS Activado,";
+      if(lat != 0){
+        textoURl = "https://www.google.com/maps?q="+lat+","+lon;
+      }
+      if (SMS){
+        SMS.sendSMS(fono, mensajetexto + textoURl, function () { showAlert('Message sent successfully');}, function (e) { showAlert('Message Failed:' + e);});
+      }
+    }
+  }
+}
 
-            $.ajax({
-            cache: false,
-            // puede ser GET, POST
-            type: "POST",  							
-            // Tipo de retorno
-            dataType: "html",
-            // pagina php que recibe la llamada
-            url: "http://72.14.183.67/ws/foto.php",  							
-            // datos, ej: $_POST['data']
-            data: {
-                    user:user,
-                    foto:fotoSrc				
-            },
-            /*beforeSend: function(){  
-                document.getElementById('divCargando').style.display="block";
-                $("#labelCargando").html('Cargando...');	
-            },*/
-            // acciones cuando me retorna algo el PHP
-            success: function( msg){
-                   console.log(msg);
-                    if(msg=='1')
-                    {
-                        showAlert('Ha ocurrido un Error. Archivo ya existe!');
-                    }
-                    else
-                    {
-                        showAlert('Foto Subida!.');
+function getImage(source){
+  // Retrieve image file location from specified source
+  navigator.camera.getPicture(uploadPhoto, onFail, { quality: 50,
+  destinationType: Camera.DestinationType.DATA_URL, sourceType: source});	//destinationType: navigator.camera.DestinationType.FILE_URI
+}
 
-                    }
-            },							
-            // acciones cuando hay error en comunicacion el el php
-            error: function(xhr, status,msg2 ){
-                    //alert('4');			
-                    console.log(xhr);
-            }
-            });//fin ajax
-	
-        }
+function onFail(message){
+  clearCache();
+  //alert('Captura Descartada.');
+  showAlert('Captura Descartada.'+ message);
+}
+
+function uploadPhoto(imageURI){
+  var largeImage = document.getElementById('largeImage');
+  largeImage.style.display = 'block';
+  largeImage.src ="data:image/jpeg;base64," + imageURI;
+}
+
+
+function enviaFoto(){
+  var user=document.getElementById('user').value;
+  //var foto=document.getElementById('largeImage');
+  var fotoSrc=document.getElementById('largeImage').src;
+
+  if(user=='' || fotoSrc==''){
+    showAlert('Debe Ingresar los valores!');
+  }
+  else{
+  //var fotoCod=encodeImageFileAsURL(foto);
+  $.ajax({
+    cache: false,
+    type: "POST", // puede ser GET, POST
+    dataType: "html", // Tipo de retorno
+    url: "http://72.14.183.67/ws/foto.php", // pagina php que recibe la llamada
+    data: {// datos, ej: $_POST['data']
+      user:user,
+      foto:fotoSrc
+    },
+    /*beforeSend: function(){
+    document.getElementById('divCargando').style.display="block";
+    $("#labelCargando").html('Cargando...');
+    },*/
+    // acciones cuando me retorna algo el PHP
+    success: function(msg){
+      console.log(msg);
+      if(msg=='1'){
+        showAlert('Ha ocurrido un Error. Archivo ya existe!');
+      }
+      else{
+        showAlert('Foto Subida!.');
+      }
+    },
+    // acciones cuando hay error en comunicacion el el php
+    error: function(xhr, status,msg2 ){
+      //alert('4');
+      console.log(xhr);
+    }
+  });//fin ajax
+  }
 }
